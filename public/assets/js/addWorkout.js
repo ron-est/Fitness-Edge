ff78de689c5d0b7f78aa824a35d859364e0a13b9
Richard Shaw <richardmorganshaw@gmail.com>
2020-12-09 11:48:02 -0800

    Updated adding workotus

--- public/assets/js/addWorkout.js
+++ public/assets/js/addWorkout.js
@@ -1,3 +1,9 @@
+let workouts = JSON.parse(localStorage.getItem('fitness_edge_custom_workouts'))
+if (!workouts) {
+  workouts = []
+  localStorage.setItem('fitness_edge_custom_workouts', JSON.stringify(workouts))
+}
+
 const validateAddWorkout = ({ title, muscle, sets, reps, weight }) => {
   if (title === '') {
     return false
@@ -41,19 +47,54 @@ document.getElementById('addWorkout').addEventListener('click', event => {
   }
 
   if (validateAddWorkout(workout)) {
-    axios.post('/api/workouts', workout)
-      .then(() => {
-        document.getElementById('title').value = ''
-        document.getElementById('muscle').value = ''
-        document.getElementById('sets').value = ''
-        document.getElementById('reps').value = ''
-        document.getElementById('weight').value = ''
-        document.getElementById('description').value = ''
-        document.getElementById('warning').textContent = ''
-        location.reload()
-      })
-      .catch(err => { console.log() })
+    workouts.push(workout)
+    localStorage.setItem('fitness_edge_custom_workouts', JSON.stringify(workouts))
+    document.getElementById('title').value = ''
+    document.getElementById('muscle').value = ''
+    document.getElementById('sets').value = ''
+    document.getElementById('reps').value = ''
+    document.getElementById('weight').value = ''
+    document.getElementById('description').value = ''
+    document.getElementById('warning').textContent = ''
+    location.reload()
+
+    // axios.post('/api/workouts', workout)
+    //   .then(() => {
+    //     document.getElementById('title').value = ''
+    //     document.getElementById('muscle').value = ''
+    //     document.getElementById('sets').value = ''
+    //     document.getElementById('reps').value = ''
+    //     document.getElementById('weight').value = ''
+    //     document.getElementById('description').value = ''
+    //     document.getElementById('warning').textContent = ''
+    //     location.reload()
+    //   })
+    //   .catch(err => { console.log() })
   } else {
     document.getElementById('warning').textContent = "Required inputs must be filled."
   }
 })
+
+let list = workouts.map(({ title, muscle, sets, reps, weight, description }) => {
+  let htmlText = `
+      <li>
+      <p>Title: ${title}</p>
+      <p>Muscle: ${muscle}</p>
+      <p>Sets: ${sets}</p>
+      <p>Reps: ${reps}</p>
+      `
+
+  if (weight) {
+    htmlText += `<p>Weight: ${weight}</p>`
+  }
+  if (description) {
+    htmlText += `
+        <label>Description:</label> 
+        <p>${description}</p>`
+  }
+  htmlText += '</li>'
+
+  return htmlText
+})
+
+document.getElementById('customWorkouts').innerHTML = list.join('')
